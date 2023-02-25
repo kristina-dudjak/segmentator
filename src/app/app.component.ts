3c955905d6a8ca8568aa6bb8bf86cfe4d18e25b0
Kristina Dudjak <dudjakkristina@gmail.com>
2023-02-25 13:27:54 +0100

    feat: draw lines on mouse drag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,82 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild
-} from '@angular/core'
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core'
+import * as d3 from 'd3'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  loadedImage: HTMLImageElement
-  @ViewChild('canvas') canvas: ElementRef<HTMLCanvasElement>
+export class AppComponent implements AfterViewInit {
+  @ViewChild('canvas') canvasRef!: ElementRef
+  private canvas!: HTMLCanvasElement
+  private context!: CanvasRenderingContext2D
+  private isDrawing = false
+  private lines: any[] = []
+  private currentLine: any
+  private isImageLoaded = false
 
-  draw () {
-    const canvas = this.canvas.nativeElement
-    const context = canvas.getContext('2d')
-    let isDrawing = false
-    canvas.width = 1000
-    canvas.height = 500
-    let lastX = 0
-    let lastY = 0
-    let color = 'red'
+  constructor () {}
+
+  ngAfterViewInit (): void {
+    this.canvas = this.canvasRef.nativeElement
+    this.context = this.canvas.getContext('2d')!
+    this.canvas.width = 1000
+    this.canvas.height = 500
+    this.isImageLoaded = false
 
     const img = new Image()
     img.src = '../assets/background.png'
     img.onload = () => {
-      context.drawImage(img, 0, 0)
+      this.isImageLoaded = true
+      this.context.drawImage(img, 0, 0)
     }
-
-    canvas.addEventListener('mousedown', event => {
-      isDrawing = true
-      ;[lastX, lastY] = [event.offsetX, event.offsetY]
-    })
-
-    canvas.addEventListener('mousemove', event => {
-      if (!isDrawing) return
-      context.beginPath()
-      context.moveTo(lastX, lastY)
-      context.lineTo(event.offsetX, event.offsetY)
-      context.strokeStyle = color
-      context.stroke()
-      ;[lastX, lastY] = [event.offsetX, event.offsetY]
-    })
-
-    canvas.addEventListener('mouseup', () => {
-      isDrawing = false
-    })
-
-    canvas.addEventListener('mouseout', () => {
-      isDrawing = false
-    })
+    this.canvas.addEventListener('mousedown', this.startDrawing.bind(this))
+    this.canvas.addEventListener('mousemove', this.draw.bind(this))
+    this.canvas.addEventListener('mouseup', this.endDrawing.bind(this))
+    this.canvas.addEventListener('mouseleave', this.endDrawing.bind(this))
   }
 
-  ngAfterViewInit (): void {
-    this.draw()
+  private startDrawing (event: MouseEvent): void {
+    this.isDrawing = true
+    const [x, y] = d3.pointer(event, this.canvas)
+    this.currentLine = { start: [x, y], end: [x, y] }
   }
 
-  async ngOnInit () {}
+  private draw (event: MouseEvent): void {
+    if (!this.isDrawing) {
+      return
+    }
+    const [x, y] = d3.pointer(event, this.canvas)
+    this.currentLine.end = [x, y]
+    this.updateCanvas()
+  }
+
+  private endDrawing (): void {
+    this.isDrawing = false
+    this.lines.push(this.currentLine)
+  }
+
+  private updateCanvas (): void {
+    const img = new Image()
+    img.src = '../assets/background.png'
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.context.drawImage(img, 0, 0)
+
+    this.lines.forEach(line => {
+      this.context.strokeStyle = 'red'
+      this.context.lineWidth = 3
+      this.context.beginPath()
+      this.context.moveTo(line.start[0], line.start[1])
+      this.context.lineTo(line.end[0], line.end[1])
+      this.context.stroke()
+    })
+    if (this.isDrawing) {
+      this.context.strokeStyle = 'red'
+      this.context.lineWidth = 3
+      this.context.beginPath()
+      this.context.moveTo(this.currentLine.start[0], this.currentLine.start[1])
+      this.context.lineTo(this.currentLine.end[0], this.currentLine.end[1])
+      this.context.stroke()
+    }
+  }
 }
