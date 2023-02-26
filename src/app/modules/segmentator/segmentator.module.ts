32f5aaeeb47b86a8b3a7f5819c4ac93830bf01ca
Kristina Dudjak <dudjakkristina@gmail.com>
2023-02-26 15:31:25 +0100

    feat: add modules

---
+++ src/app/modules/segmentator/segmentator.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SegmentatorRoutingModule } from './segmentator-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SegmentatorRoutingModule
+  ]
+})
+export class SegmentatorModule { }
