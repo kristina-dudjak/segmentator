740e924a45cd1a32bffdf573b473d8ccd1e91da1
Kristina Dudjak <dudjakkristina@gmail.com>
2023-02-27 11:53:51 +0100

    feat: lazy load modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'segmentator',
+    loadChildren: () =>
+      import('./modules/segmentator/segmentator.module').then(
+        m => m.SegmentatorModule
+      )
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
