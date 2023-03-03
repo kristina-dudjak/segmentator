8719b7d91edeb06d95f9b8da5cf01b43530cf22d
Kristina Dudjak <dudjakkristina@gmail.com>
2023-03-03 23:35:42 +0100

    feat: toggle password directive

---
+++ src/app/modules/auth/directives/toggle-password.directive.ts
@@ -0,0 +1,48 @@
+import {
+  Directive,
+  ElementRef,
+  Renderer2,
+  ViewContainerRef
+} from '@angular/core'
+import { MatIcon } from '@angular/material/icon'
+import { MatButton } from '@angular/material/button'
+@Directive({
+  selector: '[appTogglePassword]'
+})
+export class TogglePasswordDirective {
+  private isShown = false
+  constructor (
+    private el: ElementRef,
+    private vcRef: ViewContainerRef,
+    private renderer: Renderer2
+  ) {
+    this.setup()
+  }
+
+  setup () {
+    const parent = this.el.nativeElement.parentNode
+
+    const matButton =
+      this.vcRef.createComponent(MatButton).instance._elementRef.nativeElement
+    const matIcon =
+      this.vcRef.createComponent(MatIcon).instance._elementRef.nativeElement
+    this.renderer.addClass(matButton, 'toggle-button')
+    this.renderer.addClass(matButton, 'button-height')
+    this.renderer.setStyle(matButton, 'line-height', '0')
+    matIcon.innerHTML = 'visibility_off'
+
+    matButton.appendChild(matIcon)
+    matButton.addEventListener('click', () => {
+      this.toggle(matIcon)
+    })
+    this.renderer.addClass(matIcon, 'toggle-icon')
+    parent.appendChild(matButton)
+  }
+
+  toggle (button: HTMLElement) {
+    this.isShown = !this.isShown
+    const type = this.isShown ? 'text' : 'password'
+    this.renderer.setProperty(this.el.nativeElement, 'type', type)
+    button.textContent = this.isShown ? 'visibility' : 'visibility_off'
+  }
+}
