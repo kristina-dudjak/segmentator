2fb6a51f1ca3cb50b6f3d483d2f34da0f7da060e
Kristina Dudjak <dudjakkristina@gmail.com>
2023-04-24 19:25:51 +0200

    feat: save users to db

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core'
 import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFirestore } from '@angular/fire/compat/firestore'
 import * as firebase from 'firebase/compat/app'
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor (private fbAuth: AngularFireAuth) {}
+  constructor (private fbAuth: AngularFireAuth, private db: AngularFirestore) {}
 
   async register (email: string, password: string) {
-    await this.fbAuth.createUserWithEmailAndPassword(email, password)
+    await this.fbAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then(async ({ user }) => {
+        await this.db
+          .collection('users')
+          .doc(user.uid)
+          .set({ email: email }, { merge: true })
+      })
     return (await this.fbAuth.getRedirectResult()).user
   }
 
@@ -18,9 +26,18 @@ export class AuthService {
   }
 
   async googleLogIn () {
-    await this.fbAuth.signInWithPopup(
-      new firebase.default.auth.GoogleAuthProvider()
-    )
+    await this.fbAuth
+      .signInWithPopup(new firebase.default.auth.GoogleAuthProvider())
+      .then(async ({ user }) => {
+        const ref = this.db.collection(`users`).doc(user.uid).ref
+        if ((await ref.get()).exists) return
+        ref.set(
+          {
+            email: user.email
+          },
+          { merge: true }
+        )
+      })
     return (await this.fbAuth.getRedirectResult()).user
   }
 
