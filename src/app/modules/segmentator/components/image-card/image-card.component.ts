9d23ecdec6c193a9facb183e9e10d4ab8553fc88
Kristina Dudjak <dudjakkristina@gmail.com>
2023-04-26 11:57:14 +0200

    feat: add rect drawing

--- src/app/modules/segmentator/components/image-card/image-card.component.ts
+++ src/app/modules/segmentator/components/image-card/image-card.component.ts
@@ -3,6 +3,8 @@ import {
   Component,
   ElementRef,
   Input,
+  OnChanges,
+  SimpleChanges,
   ViewChild
 } from '@angular/core'
 
@@ -11,16 +13,36 @@ import {
   templateUrl: './image-card.component.html',
   styleUrls: ['./image-card.component.scss']
 })
-export class ImageCardComponent implements AfterViewInit {
+export class ImageCardComponent implements AfterViewInit, OnChanges {
   @ViewChild('canvas') canvasRef!: ElementRef
   @Input() tool!: any
   private canvas!: HTMLCanvasElement
   points: number[][] = []
+  rectPoints: number[][] = []
 
   ngAfterViewInit () {
     this.canvas = this.canvasRef.nativeElement
     this.canvas.addEventListener('mousedown', event => {
       this.tool.tool.draw(event, this.canvas, this.points)
     })
+  }
+  ngOnChanges (changes: SimpleChanges) {
+    if (this.canvas) {
+      switch (this.tool.tool.constructor.name) {
+        case 'LineTool': {
+          console.log('line')
+          this.canvas.addEventListener('mousedown', event => {
+            this.tool.tool.draw(event, this.canvas, this.points)
+          })
+          break
+        }
+        case 'RectTool': {
+          console.log('rect')
+          this.canvas.addEventListener('mousedown', event => {
+            this.tool.tool.draw(event, this.canvas, this.rectPoints)
+          })
+        }
+      }
+    }
   }
 }
--- src/app/modules/segmentator/services/rect-tool.service.ts
+++ src/app/modules/segmentator/services/rect-tool.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from '@angular/core'
 import { Tool } from '../models/Tool'
-
+import * as d3 from 'd3'
 @Injectable({
   providedIn: 'root'
 })
 export class RectTool implements Tool {
-  constructor () {}
   icon: string = 'gesture'
   draw (event: MouseEvent, canvas: HTMLCanvasElement, points: number[][]) {
-    console.log('rect draw')
+    const context = canvas.getContext('2d')!
+    const rect = canvas.getBoundingClientRect()
+    const scaleX = canvas.width / rect.width
+    const scaleY = canvas.height / rect.height
+
+    const [x, y] = d3.pointer(event)
+    const point = [x * scaleX, y * scaleY]
+    console.log(point)
+    context.fillStyle = 'red'
+    context.lineWidth = 1
+    context.strokeStyle = 'red'
+    context.beginPath()
+    context.stroke()
+
+    const mouseMove = (event: MouseEvent) => {
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      const [x, y] = d3.pointer(event)
+      const endPoint = [x * scaleX, y * scaleY]
+      const width = endPoint[0] - point[0]
+      const height = endPoint[1] - point[1]
+
+      context.beginPath()
+      context.fillStyle = 'red'
+      context.fillRect(point[0], point[1], width, height)
+      context.stroke()
+    }
+
+    const finish = (event: MouseEvent) => {
+      canvas.removeEventListener('mousemove', mouseMove)
+      canvas.removeEventListener('mousedown', finish)
+    }
+    canvas.addEventListener('mousemove', mouseMove)
+    canvas.addEventListener('mousedown', finish)
   }
 }
